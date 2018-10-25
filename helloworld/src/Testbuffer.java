98e415821dec05d636b56f2546269be8ecb893cd
RemyGuilloux <guillouxrem@gmail.com>
2018-10-25 09:27:52 +0200

    Creation Class StringBuffer

---
+++ helloworld/src/Testbuffer.java
@@ -0,0 +1,13 @@
+
+public class Testbuffer {
+
+    public static void main(String[] args) {
+
+	StringBuffer buff = new StringBuffer();
+	buff.append("Hi, Coquinou");
+	buff.append(" ,je suis content de te voir");
+	System.out.println(buff);
+
+    }
+
+}
