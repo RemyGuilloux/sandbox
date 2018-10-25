fce39cc1c80b49bd139fe14820ec8bb5efefd5ae
RemyGuilloux <guillouxrem@gmail.com>
2018-10-25 12:27:42 +0200

    Exercises

--- helloworld/src/com/socgen/basicsreview/Application.java
+++ helloworld/src/com/socgen/basicsreview/Application.java
@@ -2,20 +2,91 @@ package com.socgen.basicsreview;
 
 public class Application {
 
+//    private static void compare() {
+//
+//	boolean cond = false;
+//
+//	System.out.println();
+//
+//	System.out.println(cond);
+//	System.out.println(true > false);
+//	System.out.println(true == false);
+//	System.out.println(true >= false);
+//	System.out.println(true <= false);
+//	System.out.println(true != false);
+//
+//    boolean cond = 10 < 0;
+//
+//    if(cond)
+//    {
+//	System.out.println("10 is not less than 0");
+//
+//    }else
+//    {
+//    }System.out.println("10 is betther than 0");
+
+    int value = 10;
+    {
+
+	if (value == 0) {
+	    System.out.println("value equal 0");
+	} else if (value == 5) {
+	    System.out.println("value equal 5");
+
+	} else {
+	    System.out.println("value equal" + value);
+	}
+    }
+
+//
+//	int value1 = 32;
+//	Integer boxed = Integer.valueOf(value1);
+//	Integer autoboxed = value1; // autoboxing
+//	
+//	int value2 = Integer.valueOf(32); // Unboxing
+//	
+//	
+//	Integer boxeValue = null;
+//	int unboxedValue = boxedValue;
+//	System.out.println(unboxedValue);
+//	System.out.println(boxedValue);
+//	
+//    }
+//
     public static void main(String[] args) {
+//
+//	String cecileFirstname = args[1];
+//	String cecileLastname = args[2];
+//	String dianaFirstname = args[3];
+//	String dianaLastname = args[3];
+//	String cecileAge = args[4];
+//
+//	Integer age = Integer.valueOf(cecileAge);
+//
+//	Person Diana = new Person(args[0], dianaFirstname);
+//	Person Cecile = new Person(cecileFirstname, cecileLastname, age.intValue());
+//
+//	Person Cecile = new Person("Cecile", "GUILLOUX", 33);
+//	Person Diana = new Person("Diana", "DUGAREVA");
+//
+//	System.out.println(Cecile);
+//	System.out.println(Diana);
+//
+//	compare();
+//	autob;oxing();
 
-	String majorityStr = args[0];
+	sum(2.2, 2.2, 2.2, 2.2, 2.2);
+    }
 
-	Integer majority = Integer.valueOf(majorityStr);
-	int maj = majority.intValue();
-	System.out.println("Majority= " + maj);
+    private static void sum(double... values) {
 
-	Person Cecile = new Person("Cecile", "GUILLOUX");
-	Person Diana = new Person("Diana", "DUGAREVA", 24);
+	double total = 0.0;
+	for (double value : values) {
+	    total += value;
 
-	System.out.println(Cecile);
-	System.out.println(Diana);
+	}
 
+	System.out.println(total);
     }
 
 }
